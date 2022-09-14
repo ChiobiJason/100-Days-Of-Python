d8ff3bd80aed4d804f85cfd7ef0d0c7e6629ab10
ChiobiJason <chiobichisom@gmail.com>
2022-09-14 17:42:49 -0600

    Finished Day 1

---
+++ Day 1/README.md
@@ -0,0 +1,3 @@
+# Day 1 of #100daysofpython
+
+Learnt about inputs, variables, string concatenation, new lines.
---
+++ Day 1/day-1-variables-start/main.py
@@ -0,0 +1,9 @@
+name = "Jack"
+print(name)
+
+name = "Jason"
+print(name)
+
+name = input("What is your name?")
+length = len(name)
+print(length)
