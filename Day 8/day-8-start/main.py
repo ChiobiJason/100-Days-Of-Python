7fe01977fc7044c96e970567a1c0c70492d1cdc0
ChiobiJason <chiobichisom@gmail.com>
2022-09-21 11:12:24 -0600

    Day 8

---
+++ Day 8/day-8-start/main.py
@@ -0,0 +1,21 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#   print("Hello")
+#   print("Bonjour")
+#   print("Hola")
+# greet()
+
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How do you do {name}?")
+# greet_with_name("Angela")
+
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}?")
+
+greet_with(location='London', name="Jason")
